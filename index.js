aea9d27b95978853765324b3b2c02d9df413d32f
forkingits1123 <lblack11235813@gmail.com>
2021-05-18 09:15:07 -0700

    commit tree 6b1876b44a56742e025528dac5136c8e7ed66717

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    let hq = 42;
+    if (distance > hq) {
+        return distance - hq;
+    } else if (distance < hq) {
+        return hq - distance;
+    }
+}
+
+function distanceFromHqInFeet(distance) {
+    let d = distanceFromHqInBlocks(distance);
+    return d * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    } else if (start < destination) { 
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400) {
+        return 0;
+    }
+    if (feet <= 2000) {
+        let price = (feet - 400) * .02; 
+        return price;   
+    } else if (feet > 2000 && feet < 2500) {
+        return 25.00;
+    } else if (feet > 2500) {
+        return 'cannot travel that far';
+    }
+}
